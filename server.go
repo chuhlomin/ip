f28857f91536fe32cf2253ecbd66fe797f44a1db
Konstantin Chukhlomin <mail@chuhlomin.com>
2021-11-25 13:31:19 -0500

    Add /<ip>/<mask> endpoint

--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/go-chi/chi/v5"
@@ -41,6 +43,7 @@ func (s *server) routes() {
 	s.router.HandleFunc("/{ip:[0-9.]+}json", s.handleIP("json"))
 	s.router.HandleFunc("/{ip:[0-9.]+}", s.handleIP("yaml"))
 	s.router.HandleFunc("/{ip:[0-9.]+}/whois", s.handleWhois())
+	s.router.HandleFunc("/{ip:[0-9.]+}/{mask:[0-9]+}", s.handleMask())
 	s.router.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		fmt.Fprintf(w, "User-agent: *\nDisallow: /\n")
@@ -123,6 +126,85 @@ func (s *server) handleWhois() http.HandlerFunc {
 
 		w.Header().Set("Content-Type", "text/plain")
 		fmt.Fprint(w, whoisRaw)
+	}
+}
+
+func (s *server) handleMask() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ipParam := chi.URLParam(r, "ip")
+		maskParam := chi.URLParam(r, "mask")
+
+		m, err := strconv.Atoi(maskParam)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if m < 0 || m > 32 {
+			log.Println("invalid mask")
+			http.Error(w, "invalid mask", http.StatusBadRequest)
+			return
+		}
+
+		_, ipv4Net, err := net.ParseCIDR(ipParam + "/" + maskParam)
+		if err != nil {
+			log.Print(err)
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprint(w, ipParam+"/"+maskParam+"\n\n")
+
+		// print IP with paddings to align with binary format
+		ipParts := strings.Split(ipParam, ".")
+		for i, part := range ipParts {
+			fmt.Fprintf(w, "%8s", part)
+			if i < len(ipParts)-1 {
+				fmt.Fprint(w, ".")
+			}
+		}
+		fmt.Fprint(w, "\n")
+
+		// print IP in binary format
+		for i, part := range ipParts {
+			p, err := strconv.Atoi(part)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			binPart := fmt.Sprintf("%08b", p)
+			fmt.Fprint(w, binPart)
+			if i < len(ipParts)-1 {
+				fmt.Fprint(w, ".")
+			}
+		}
+		fmt.Fprint(w, "\n")
+
+		// print mask
+		for i := 0; i < m; i++ {
+			fmt.Fprint(w, "X")
+			if (i+1)%8 == 0 && i < m-1 {
+				fmt.Fprint(w, ".")
+			}
+		}
+		fmt.Fprint(w, "\n\n")
+
+		if m >= 16 {
+			// print all possible IPs
+			mask := binary.BigEndian.Uint32(ipv4Net.Mask)
+			start := binary.BigEndian.Uint32(ipv4Net.IP)
+
+			// find the final address
+			finish := (start & mask) | (mask ^ 0xffffffff)
+
+			// loop through addresses as uint32
+			for i := start; i <= finish; i++ {
+				// convert back to net.IP
+				ip := make(net.IP, 4)
+				binary.BigEndian.PutUint32(ip, i)
+				fmt.Fprintln(w, ip)
+			}
+		}
 	}
 }
 
