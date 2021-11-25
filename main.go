109de8c286ed0a2311816ea0e6eb17c95bc92819
Konstantin Chukhlomin <mail@chuhlomin.com>
2021-11-24 21:53:47 -0500

    Print all headers

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ip := r.RemoteAddr
-	fmt.Fprintf(w, ip)
+	fmt.Fprintf(w, "IP: %v\n", ip)
+
+	// print all headers
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
 }
 
 func main() {
