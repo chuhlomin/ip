ae865ba105028c887227bb3d64163595192b8ca9
Konstantin Chukhlomin <mail@chuhlomin.com>
2021-11-27 00:23:12 -0500

    Add /help endpoint

--- server.go
+++ server.go
@@ -35,6 +35,44 @@ func (s *server) routes() {
 	s.router.HandleFunc("/{ip:[0-9.]+}", s.handleIP("yaml"))
 	s.router.HandleFunc("/{ip:[0-9.]+}/whois", s.handleWhois())
 	s.router.HandleFunc("/{ip:[0-9.]+}/{mask:[0-9]+}", s.handleMask())
+	s.router.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Location", "/help")
+		w.WriteHeader(http.StatusFound)
+	})
+	s.router.HandleFunc("/help", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, `ip.chuhlomin.com is a service for finding information about IP addresses.
+
+It uses:
+  * GeoLite2 databases for ASN and GeoIP lookups,
+  * whois.iana.org for Whois queries.
+
+Available endpoints:
+  /help - this page
+  / - index page, redirects to /{ip}, where {ip} is your IP address
+  /{ip} - returns information about the IP address: ASN and GeoIP
+  /{ip}/whois - returns the Whois information for the IP address
+  /{ip}/{mask} - displays the IP in binary format, visualizing the mask
+
+Example usage:
+  curl -L http://ip.chuhlomin.com/
+  curl http://ip.chuhlomin.com/1.1.1.1
+  curl http://ip.chuhlomin.com/1.1.1.1/whois
+  curl http://ip.chuhlomin.com/192.168.0.0/24
+
+Version: 1.0.0
+Source code: https://github.com/chuhlomin/ip
+Author: Konstantin Chukhlomin
+License: MIT
+
+---
+
+Known alternatives:
+  https://ifconfig.co
+  https://ipinfo.io
+  https://whatismyipaddress.com
+`)
+	})
 	s.router.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		fmt.Fprintf(w, "User-agent: *\nDisallow: /\n")
